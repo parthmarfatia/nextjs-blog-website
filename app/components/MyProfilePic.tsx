9d35f8cabd4c98b5f5cbdb5750009449cd3ff630
Parth Marfatia <parthmarfatia153@gmail.com>
2023-09-13 01:34:59 +0530

    project completed

---
+++ app/components/MyProfilePic.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function MyProfilePic() {
+  return (
+    <section className="w-full mx-auto">
+      <Image
+        className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8 object-cover w-32 h-32"
+        src={
+          "https://m.media-amazon.com/images/M/MV5BMGQ1ZGZmNTAtM2MyYi00NmZhLTkwYmYtNTNlZDRhMzU2ZTgwXkEyXkFqcGdeQW1yb3NzZXI@._V1_.jpg"
+        }
+        width={200}
+        height={200}
+        alt="Dave Gray"
+        priority={true}
+      />
+    </section>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["m.media-amazon.com"], // Add your image domain here
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
